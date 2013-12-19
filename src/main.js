b62304969f58e6ebd0c4859fca1f04cf3f66443b
Ionică Bizău <bizauionica@yahoo.com>
2013-12-19 21:52:01 +0200

    Initial release

---
+++ src/main.js
@@ -0,0 +1,26 @@
+(function ( $ ) {
+    $.fn.pOn = function (events, priority, callback) {
+            
+        var pEvents = this.data("pEvents") || [];
+        
+        pEvents.push([
+            priority,
+            callback
+        ]);
+        
+        pEvents.sort(function (e1, e2) {
+            return e1[0] - e2[0];
+        });
+        
+        this.data("pEvents", pEvents);
+        
+        if (pEvents.length > 1) { return; }
+        
+        this.on(events, selector, function () {
+            var pEvs = $(this).data("pEvents") || [];
+            for (var i = 0; i < pEvs.length; ++i) {
+                pEvs[i][1].call(this);
+            }
+        });
+    }
+})($);
